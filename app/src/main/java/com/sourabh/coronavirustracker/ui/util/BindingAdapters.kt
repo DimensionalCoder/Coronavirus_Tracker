b3a6223ae617fc6a96c8b1c7eeb529d50a99b35c
Sourabh S Nath <sourabhsnath@gmail.com>
2020-05-29 19:51:04 +0530

    Switched to Java8 DateTimeFormatter

--- app/src/main/java/com/sourabh/coronavirustracker/ui/util/BindingAdapters.kt
+++ app/src/main/java/com/sourabh/coronavirustracker/ui/util/BindingAdapters.kt
@@ -6,7 +6,8 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.google.android.material.snackbar.Snackbar
-import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 // Using SimpleDateFormat since Java 8 date time classes require min api 26
 @SuppressLint("SimpleDateFormat")
@@ -15,18 +16,12 @@ fun TextView.setUpdateTime(updateTime: String?) {
     updateTime?.let {
         val dateTime = updateTime.split(" ")
         val time = dateTime[1]
-
-        val tf = SimpleDateFormat("HH:mm:ss")
-        val twelve = SimpleDateFormat("hh:mm a")
-        val formattedTime = twelve.format(tf.parse(time)!!)
-
         val date = dateTime[0]
-        val format = SimpleDateFormat("dd/MM/yyyy")
-        val stringDate = SimpleDateFormat("dd MMM yyyy")
-        val formattedDate = stringDate.format(format.parse((date))!!)
 
-        val dateTimeText = "$formattedTime, $formattedDate"
-        text = dateTimeText
+        text = LocalDateTime.parse(
+            "$time $date",
+            DateTimeFormatter.ofPattern("HH:mm:ss dd/MM/yyyy")
+        ).format(DateTimeFormatter.ofPattern("hh:mm a, dd MMM yyyy"))
     }
 }
 
