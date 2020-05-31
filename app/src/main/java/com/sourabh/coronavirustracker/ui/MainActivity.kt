900d4d390caa5ea935c0b02d64e7ee20aaf2881c
Sourabh S Nath <sourabhsnath@gmail.com>
2020-05-31 08:51:16 +0530

    Moved val binding

--- app/src/main/java/com/sourabh/coronavirustracker/ui/MainActivity.kt
+++ app/src/main/java/com/sourabh/coronavirustracker/ui/MainActivity.kt
@@ -10,12 +10,10 @@ import com.sourabh.coronavirustracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val bottomAppBar = binding.bar
